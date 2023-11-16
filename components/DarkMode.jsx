e2c1b237167063524a4e8119fdcb6047fc012cc4
bonzo-coder <113475630+bonzo-coder@users.noreply.github.com>
2023-11-16 22:13:45 +0100

    Toggle dark working. Set min width to 20%

--- components/DarkMode.jsx
+++ components/DarkMode.jsx
@@ -3,24 +3,20 @@ import React from "react"
 export default function ToggleDark () {
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
     const currentTheme = localStorage.getItem('theme');
-
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
     
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
-    }
-    console.log(e.target)
-    function switchTheme(e) {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-        else {        document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }    
-    }
+    
 
-    toggleSwitch.addEventListener('change', switchTheme, false);
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleSwitch.checked = false;
+    }
+    else {        
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleSwitch.checked = true;
+    }    
+    
+
+   // toggleSwitch.addEventListener('change', switchTheme, false);
 }
